379e75054536e053499aec6a3481f1de9749d298
JavaDevMZ <140167480+JavaDevMZ@users.noreply.github.com>
2025-05-20 23:37:08 +0200

    Refactor ContactEmployeeController

--- src/main/java/gov/milove/main/controller/ContactEmployeeController.java
+++ src/main/java/gov/milove/main/controller/ContactEmployeeController.java
@@ -9,24 +9,19 @@ import java.util.List;
 
 @Tag(name="Contact employee controller")
 @RestController
-@RequestMapping("/api")
 public interface ContactEmployeeController {
 
     @Operation(summary = "Get the available employees")
-    @GetMapping("/contacts")
     List<ContactEmployee> getAll();
 
     @Operation(summary = "Add an employee")
-    @PostMapping("/protected/contact/new")
     ContactEmployee create(@RequestBody ContactEmployee employee);
 
     @Operation(summary = "Update an employee")
-    @PostMapping("/contact/update")
     Long update(
             @RequestParam("id") Long id,
             @RequestBody ContactEmployee updatedEmployee);
 
     @Operation(summary = "Delete an employee")
-    @DeleteMapping("/protected/contact/{id}/delete")
     Long delete(@PathVariable Long id);
 }
--- src/main/java/gov/milove/main/controller/impl/ContactEmployeeControllerImpl.java
+++ src/main/java/gov/milove/main/controller/impl/ContactEmployeeControllerImpl.java
@@ -4,30 +4,34 @@ import gov.milove.main.controller.ContactEmployeeController;
 import gov.milove.main.domain.ContactEmployee;
 import gov.milove.main.repository.jpa.ContactEmployeeRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api")
 public class ContactEmployeeControllerImpl implements ContactEmployeeController {
 
     private final ContactEmployeeRepository repository;
 
 
     @Override
+    @GetMapping("/contacts")
     public List<ContactEmployee> getAll() {
         return repository.findAll();
     }
 
 
     @Override
+    @PostMapping("/protected/contact/new")
     public ContactEmployee create(ContactEmployee employee) {
         repository.save(employee);
         return employee;
     }
 
     @Override
+    @PostMapping("/contact/update")
     public Long update(Long id, ContactEmployee updatedEmployee) {
         updatedEmployee.setId(id);
         repository.save(updatedEmployee);
@@ -35,6 +39,7 @@ public class ContactEmployeeControllerImpl implements ContactEmployeeController 
     }
 
     @Override
+    @DeleteMapping("/protected/contact/{id}/delete")
     public Long delete(Long id) {
         repository.deleteById(id);
         return id;
