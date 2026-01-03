cc1e49c6e91aa99a444ad9a72011120e710641ad
JavaDevMZ <140167480+JavaDevMZ@users.noreply.github.com>
2026-01-03 20:45:04 +0100

    Change current user retrieval logic to match OAuth2

--- src/main/java/gov/milove/main/service/impl/AppUserServiceImpl.java
+++ src/main/java/gov/milove/main/service/impl/AppUserServiceImpl.java
@@ -7,8 +7,11 @@ import gov.milove.main.service.AppUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,11 +22,19 @@ public class AppUserServiceImpl implements AppUserService {
     public AppUser getCurrentUser() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
-        if (auth instanceof JwtAuthenticationToken) {
-            String email = ((JwtAuthenticationToken) auth).getToken().getClaim("email");
-            return repository.findByEmail(email).orElseThrow(() -> new AppUserNotFoundException("Current user not found"));
+        if (auth != null && auth.getPrincipal() instanceof OAuth2User oauthUser) {
+            String email = oauthUser.getAttribute("email");
+            String googleId = oauthUser.getAttribute("sub");
+            Optional<AppUser> user = repository.findById(googleId);
+            if (user.isPresent()) {
+                return user.get();
+            }
+            else {
+                return repository.findByEmail(email).orElseThrow(() -> new AppUserNotFoundException("Current user not found"));
+            }
+        } else {
+            throw new AppUserNotFoundException("Current user not found");
         }
-        return repository.findByEmail(auth.getName()).orElseThrow(() -> new AppUserNotFoundException("Current user not found"));
     }
 
 }
