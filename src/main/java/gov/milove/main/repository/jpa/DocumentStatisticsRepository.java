b6b5b3b6bdde8b589dbf83df36180da35f168bed
JavaDevMZ <140167480+JavaDevMZ@users.noreply.github.com>
2026-01-04 12:58:30 +0100

    Fix document report retrieval query to work with null group and author

--- src/main/java/gov/milove/main/repository/jpa/DocumentStatisticsRepository.java
+++ src/main/java/gov/milove/main/repository/jpa/DocumentStatisticsRepository.java
@@ -13,20 +13,20 @@ public interface DocumentStatisticsRepository extends JpaRepository<DocumentRetr
 
     @Query("""
                select new gov.milove.main.dto.DocumentReportItemDto(
-                                                                d.id,
-                                                                d.title,
-                                                                dg.name,
-                                                                sum(case when dr.action = 'DOWNLOAD' then 1 else 0 end),
-                                                                sum(case when dr.action = 'VIEW' then 1 else 0 end),
-                                                                u.email
-                                                            )
-                                                            from Document d
-                                                            join d.documentGroup dg
-                                                            join d.addedBy u
-                                                            left join DocumentRetrieval dr
-                                                                on dr.createdOn between :start and :end
-                                                            group by d.id, d.title, dg.name, u.email
-            """)
+                   d.id,
+                   d.title,
+                   dg.name,
+                   sum(case when dr.action = 'DOWNLOAD' then 1 else 0 end),
+                   sum(case when dr.action = 'VIEW' then 1 else 0 end),
+                   u.email
+               )
+               from Document d
+               left join d.documentGroup dg
+               left join d.addedBy u
+               join DocumentRetrieval dr on dr.document.id = d.id
+               and dr.createdOn between :start and :end
+               group by d.id, d.title, dg.name, u.email
+           """)
     List<DocumentReportItemDto> findDocumentStatisticsByCreatedOnBetween(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
 
 }
