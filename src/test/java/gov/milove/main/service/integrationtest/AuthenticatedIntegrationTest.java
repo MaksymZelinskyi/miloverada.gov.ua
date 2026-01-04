7b38188e4ce3e30bfdcebb9a050d8eaf05ec5240
JavaDevMZ <140167480+JavaDevMZ@users.noreply.github.com>
2026-01-04 14:41:30 +0100

    Rearrange tests

--- src/test/java/gov/milove/main/service/integrationtest/AuthenticatedIntegrationTest.java
+++ src/test/java/gov/milove/main/service/integrationtest/AuthenticatedIntegrationTest.java
@@ -1,4 +1,4 @@
-package gov.milove.main.controller.integrationtest;
+package gov.milove.main.service.integrationtest;
 
 import gov.milove.config.IntegrationTest;
 import gov.milove.main.domain.AppUser;
@@ -10,49 +10,33 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class AuthenticatedIntegrationTest extends IntegrationTest {
 
 
+    protected final String TEST_EMAIL = "admin@email.com";
     @Autowired
     protected AppUserRepository repo;
-
     protected AppUser user;
-    protected final String TEST_EMAIL = "admin@email.com";
 
     @BeforeEach
     public void createUser() {
-        this.user = repo.save(AppUser.builder()
-                .id("user-1")
-                .email(TEST_EMAIL)
-                .firstName("John")
-                .lastName("Doe")
-                .avatarUrl("")
-                .build());
+        this.user = repo.save(AppUser.builder().id("user-1").email(TEST_EMAIL).firstName("John").lastName("Doe").avatarUrl("").build());
 
         Map<String, Object> attributes = new HashMap<>();
         attributes.put("sub", "user-1");
         attributes.put("email", TEST_EMAIL);
 
         // Create OAuth2User with authorities
-        OAuth2User oauth2User = new DefaultOAuth2User(
-                Collections.singletonList(new SimpleGrantedAuthority("admin")),
-                attributes,
-                "sub" // name attribute key
+        OAuth2User oauth2User = new DefaultOAuth2User(Collections.singletonList(new SimpleGrantedAuthority("admin")), attributes, "sub" // name attribute key
         );
 
         // Create OAuth2AuthenticationToken
-        OAuth2AuthenticationToken token = new OAuth2AuthenticationToken(
-                oauth2User,
-                oauth2User.getAuthorities(),
-                "google" // client registration id
+        OAuth2AuthenticationToken token = new OAuth2AuthenticationToken(oauth2User, oauth2User.getAuthorities(), "google" // client registration id
         );
         SecurityContextHolder.getContext().setAuthentication(token);
     }
